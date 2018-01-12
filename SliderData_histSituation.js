1def07271b8a070f695040881d764205d814f045
ferremar <ferremar@gmx.ch>
2018-01-12 13:18:00 +0100

    2

---
+++ SliderData_histSituation.js
@@ -0,0 +1,7 @@
+window.sliderData = {
+    13_1_histSituation:"....1",
+    13_2_histSituation:"....2",
+    13_3_histSituation:"....3",
+    13_4_histSituation:"....4",
+    13_5_histSituation:"Schloss Greifenstein"
+}
